d7179d0917f7023d81a9a40d2a00338cd339c6ed
hm06063 <54925126+hm06063@users.noreply.github.com>
2020-06-13 21:51:05 -0700

    Update final.py
    text crawling and store words

--- final.py
+++ final.py
@@ -1,6 +1,19 @@
 #! /usr/bin/python
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
+import re, requests
+from bs4 import BeautifulSoup
+import operator
+from elasticsearch import Elasticsearch
+import nltk
+from nltk import punkt
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+
+es_host = "127.0.0.1"
+es_port = "9200"
+
 app=Flask(__name__)
 
 url_list=[]
@@ -35,6 +48,69 @@ def upload_file() :
 					continue;
 				url_list.append(line)
 			fp.close()
+			
+	es = Elasticsearch([{'host':es_host,'port':es_port}],timeout=30)
+
+	stop_words = set(stopwords.words('english'))
+	
+	i=0
+	for url in url_list:
+		
+		word_d = {}
+
+		res = requests.get(url)
+		html = res.text
+		soup = BeautifulSoup(html,'html.parser',from_encoding='utf-8')
+		for s in soup('script'): s.extract()
+		for s in soup('style'): s.extract()
+		my_para = soup.select('body div')
+		
+			
+		for para in my_para : 
+
+			sen = para.get_text()
+			sen = sen.lower().replace(',',' ').replace('.',' ')
+			#translator = str.maketrans('','',string.punctuation)
+			#sen = sen.translate(translator)
+			sen = re.sub('[^0-9a-zA-Zㄱ-힗]', '', sen)
+	
+			tokens = word_tokenize(sen)
+						
+			words = []
+			for word in tokens:
+				word = str(filter(str.isalpha,word))
+				words.append(word)
+	
+			wlist = [k for k in words if not k in stop_words]
+			#wlist = sen.split()
+
+			for w in wlist:
+				if w not in word_d:
+					word_d[w]=1
+				word_d[w]+=1
+	
+		count = 0
+		for w,c in sorted(word_d.items(),key=lambda x:x[1], reverse=False):
+			count += c
+
+		
+		top_words = []
+		similar_urls = []
+
+		e = {
+			"url":url,
+			"words_num":count,
+			"words":word_d,
+			"top_words":top_words,
+			"similar_urls":similar_urls
+		}
+		res = es.index(index='urls',doc_type='analysis',id=i,body=e)
+		
+		i+=1
+		
+		print(res)
+
+	
 	return render_template("upload.html", len=len(url_list), url_list=url_list, num_word=num_word, time=time)
 		
 
